391a372cc075440ae308ab7901ac304263eace94
user <user@users-MacBook-Pro.local>
2022-06-08 11:09:06 -0700

    Added UL dropdown menus to each Nav LI item. Added styling and then added function for these dropdown to display when the corresponding LI item is selected. This only works on click the item and will remove dropdown when clicked again. Will work on dropdown disapperaing when if clicked outside of element and not just by clicking item again

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', ()=> {
   let countTag = document.querySelector('.count p');
+  const navHead = document.querySelector('nav');
   let count = 100;
+
+  /** 
+   * Will remove call classNames from the selected target, does not remove a single className only for list
+   * @param target list of Element tags
+  */
+  function removeClassNameAll(target) {
+    for(let i=0; i<target.length;i++) {
+      let targetClassRemove = target[i];
+      targetClassRemove.classList.remove('visible');
+    }
+  }
+
   countTag.innerText = count;
   setInterval(()=> {
     count -= 1;
@@ -11,6 +24,24 @@ document.addEventListener('DOMContentLoaded', ()=> {
       //@clearInterval()
     }
   }, 1000);
+
+  navHead.addEventListener('click', (e)=> {
+    let target = e.target
+    if(target.tagName === 'SPAN') {
+    if(target.closest('NAV') === navHead) {
+      const dropDownULs = navHead.querySelectorAll('li ul');
+      let targetDropDown = target.nextElementSibling;
+      //If target drop down is seleted the dropdown menu for that target will be displayed
+      if(!targetDropDown.classList.value.includes('visible')) {
+      removeClassNameAll(dropDownULs);
+      targetDropDown.classList.add('visible');
+      //If target clicked again will remove that targets dropdown menu
+    } else {
+      targetDropDown.classList.remove('visible');
+    }
+    }
+  }
+  })
 
 })
 
