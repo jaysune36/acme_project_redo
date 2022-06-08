1976b28b343bd46d97104bc1d77bf6ce039b045c
user <user@users-MacBook-Pro.local>
2022-06-08 09:48:19 -0700

    created countdown within timer to count down to 100, right now it recycles back to 100 secs once it hits zero, but will make this stop at zero and a sale banner stating closed will appear. Added hover affect to display img product tag with individual product names.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+document.addEventListener('DOMContentLoaded', ()=> {
+  let countTag = document.querySelector('.count p');
+  let count = 100;
+  countTag.innerText = count;
+  setInterval(()=> {
+    count -= 1;
+    countTag.innerText = count;
+    if(count < 1) {
+      count = 100;
+      // Will keep this code, might decide to kill internval after 100 secs has passed
+      //@clearInterval()
+    }
+  }, 1000);
+
+})
+
