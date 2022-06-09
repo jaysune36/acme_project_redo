0ffca8dbc814d360eabb35a387f22f0daba1d869
user <user@users-MacBook-Pro.local>
2022-06-09 10:08:25 -0700

    Finialized search function. When searching will use compared what is typed to JSON items. If the items matches it will appear and display a dropdown menu. When make would longer this will adjust the list and display a new list. Added item to window Eventlistner for when clicked outside of scope to close the dropdown menu in search.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 
 
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
   let countTag = document.querySelector('.count p');
   const navHead = document.querySelector('nav');
   const search = document.getElementById('search-bar');
   let count = 100;
   const searchArr = [];
 
+  /**
+   * Creates Arr from JSON to use when searching
+   * @param {data} - JSON data passed 
+   */
   function generateDir(data) {
     data.map((searchValue) => {
       let itemName = searchValue.name;
@@ -14,29 +18,28 @@ document.addEventListener('DOMContentLoaded', ()=> {
     })
   }
 
+  // Gathers JSON file and pushes to generateDir()
   fetch("../app.json")
     .then(data => data.json())
     .then(generateDir)
-    .catch(err=>console.log(err));
-
-  console.log(searchArr);
+    .catch(err => console.log(err));
 
   /** 
    * Will remove call classNames from the selected target, does not remove a single className only for list
-   * @param target list of Element tags
+   * @param {target} - Arr of Tags
   */
   function removeClassNameAll(target) {
-    for(let i=0; i<target.length;i++) {
+    for (let i = 0; i < target.length; i++) {
       let targetClassRemove = target[i];
       targetClassRemove.classList.remove('visible');
     }
   }
 
   countTag.innerText = count;
-  setInterval(()=> {
+  setInterval(() => {
     count -= 1;
     countTag.innerText = count;
-    if(count < 1) {
+    if (count < 1) {
       count = 100;
       // Will keep this code, might decide to kill internval after 100 secs has passed
       //@clearInterval()
@@ -44,51 +47,68 @@ document.addEventListener('DOMContentLoaded', ()=> {
   }, 1000);
 
 
-  window.addEventListener('mouseup', (e)=> {
-    if(e.target != navHead && e.target.parentNode != navHead) {
+  window.addEventListener('mouseup', (e) => {
+    // Closes dropdown menu if clicked outside of drop down
+    if (e.target != navHead && e.target.parentNode != navHead) {
       const dropDownULs = navHead.querySelectorAll('li ul');
       removeClassNameAll(dropDownULs);
     }
-    if(e.target != search && e.target.parentNode != search) {
+    // Closes search dropdown menu if clicked outside of search
+    if (e.target != search && e.target.parentNode != search) {
       document.getElementById('search-value').removeAttribute('style');
+      search.querySelector('.search-box ul').style.display = 'none';
+      search.querySelector('.search-box ul').innerHTML = '';
     }
   });
 
-  navHead.addEventListener('click', (e)=> {
+  navHead.addEventListener('click', (e) => {
     let target = e.target
-    if(target.tagName === 'SPAN') {
-    if(target.closest('NAV') === navHead) {
-      const dropDownULs = navHead.querySelectorAll('li ul');
-      let targetDropDown = target.nextElementSibling;
-      //If target drop down is seleted the dropdown menu for that target will be displayed
-      if(!targetDropDown.classList.value.includes('visible')) {
-      removeClassNameAll(dropDownULs);
-      targetDropDown.classList.add('visible');
-      //If target clicked again will remove that targets dropdown menu
-    } else {
-      targetDropDown.classList.remove('visible');
-    }
-    }
-  }
-  })
-  search.addEventListener('click', (e)=> {
-    let target = e.target;
-    if(target.tagName === 'INPUT') {
-    if(!target.style.boxShadow) {
-    target.style.boxShadow = '0px 0px 4px 1px #D84035';
-    }
-  }
-  });
-  search.addEventListener('keyup', (e)=> {
-    let target=e.target;
-    for(let i=0;i<searchArr.length;i++){
-      let arrItem = searchArr[i];
-      if(arrItem.toLowerCase().includes(target.value)) {
-        let dropdownItem = '';
-        dropdownItem += arrItem;
-        console.log(dropdownItem);
+    if (target.tagName === 'SPAN') {
+      if (target.closest('NAV') === navHead) {
+        const dropDownULs = navHead.querySelectorAll('li ul');
+        let targetDropDown = target.nextElementSibling;
+        //If target drop down is seleted the dropdown menu for that target will be displayed
+        if (!targetDropDown.classList.value.includes('visible')) {
+          removeClassNameAll(dropDownULs);
+          targetDropDown.classList.add('visible');
+          //If target clicked again will remove that targets dropdown menu
+        } else {
+          targetDropDown.classList.remove('visible');
+        }
       }
-  }
+    }
+  })
+  search.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target.tagName === 'INPUT') {
+      if (!target.style.boxShadow) {
+        target.style.boxShadow = '0px 0px 4px 1px #D84035';
+      }
+    }
+  });
+  search.addEventListener('keyup', (e) => {
+    let target = e.target;
+    const searchBox = search.querySelector('.search-box');
+    const searchUl = searchBox.querySelector('ul');
+    if (searchBox.querySelector('input').value) {
+      searchUl.style.display = 'block';
+      if(searchUl.childElementCount === 10) {
+        searchUl.innerHTML = '';
+      }
+      for (let i = 0; i < searchArr.length; i++) {
+        let arrItem = searchArr[i];
+        if (arrItem.toLowerCase().includes(target.value)) {
+          let dropdownItem = '';
+          dropdownItem += arrItem;
+          searchUl.innerHTML += `
+        <li>${dropdownItem}</li>
+        `;
+          if(searchUl.querySelectorAll('li').length === 10) {
+            break;
+          }
+        }
+      }
+    }
   })
 
 })
